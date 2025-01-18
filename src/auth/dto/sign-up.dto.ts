3427f7e6bcbe829d34ae18c17e363b4781064c20
Urus <105210804+sampiev@users.noreply.github.com>
2025-01-18 04:49:38 +0300

    Rename and delete files

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,6 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthDto } from './sign-in.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {
-  id: number;
-}
