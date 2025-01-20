6a4931f67904c0463d9fecf98e6d01eb71827df2
Urus <105210804+sampiev@users.noreply.github.com>
2025-01-20 20:30:31 +0300

    add: redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SmsAuthModule } from './sms-auth/sms-auth.module';
+import { RedisModule } from './redis/redis.module';
 
 
 
 
 @Module({
-  imports: [UsersModule, SmsAuthModule],
+  imports: [UsersModule, SmsAuthModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
