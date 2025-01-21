8410422c6f46defb1c7f8d126e190527f70f3de1
Urus <105210804+sampiev@users.noreply.github.com>
2025-01-21 04:18:02 +0300

    redis done

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
     controllers: [UsersController],
     providers: [PrismaService, UsersService],
     exports: [UsersService],
+    imports: [RedisModule],
 })
 export class UsersModule { }
 
